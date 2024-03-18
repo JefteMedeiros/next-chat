a9f968626cbe0be63d6dd410da02d6b2983ca1fd
Jefté Medeiros <jefte.medeiros8@gmail.com>
2024-03-18 18:15:00 -0500

    added sign out functionality

---
+++ components/sign-out-button.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useState } from "react"
+import { LoaderIcon, LogOut } from "lucide-react"
+import { signOut } from "next-auth/react"
+
+import { Button } from "./ui/button"
+import { useToast } from "./ui/use-toast"
+
+export function SignOutButton() {
+  const { toast } = useToast()
+
+  const [isSigningOut, setIsSigningOut] = useState(false)
+
+  return (
+    <Button
+      variant="ghost"
+      className="gap-1 h-full aspect-square"
+      disabled={isSigningOut}
+      onClick={async () => {
+        setIsSigningOut(true)
+
+        try {
+          await signOut()
+        } catch (error) {
+          toast({
+            title: "There was a problem signing out",
+          })
+        } finally {
+          setIsSigningOut(false)
+        }
+      }}
+    >
+      {isSigningOut && <LoaderIcon size={16} />}
+      <LogOut />
+    </Button>
+  )
+}
