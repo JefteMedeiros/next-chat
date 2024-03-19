d9109e8e09d43b404b65f90c284cd78148c2a119
Jefté Medeiros <jefte.medeiros8@gmail.com>
2024-03-18 23:28:53 -0500

    added feature to add friend

---
+++ app/api/friend/accept/route.ts
@@ -0,0 +1,54 @@
+import { getServerSession } from "next-auth"
+import { z } from "zod"
+
+import { fetchRedis } from "@/helpers/redis"
+import { authOptions } from "@/lib/auth"
+import { db } from "@/lib/db"
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+
+    const { id: idToAdd } = z.object({ id: z.string() }).parse(body)
+
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+      return new Response("Unauthorized", {
+        status: 401,
+      })
+    }
+
+    const isAlreadyFriends = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd,
+    )
+
+    if (isAlreadyFriends) {
+      return new Response("Already friends", {
+        status: 400,
+      })
+    }
+
+    const hasFriendRequest = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:incoming_friend_requests`,
+      idToAdd,
+    )
+
+    if (!hasFriendRequest) {
+      return new Response("No friend request", {
+        status: 400,
+      })
+    }
+
+    await db.sadd(`user:${session.user.id}:friends`, idToAdd)
+
+    await db.sadd(`user:${idToAdd}:friends`, session.user.id)
+
+    return new Response("OK")
+  } catch (error) {
+    console.log(error)
+  }
+}
